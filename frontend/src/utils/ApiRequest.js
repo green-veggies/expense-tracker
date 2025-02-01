8e963f0ee9fa44dc77c40382a6f828da2129b91c
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2025-02-02 03:07:56 +0530

    package.json updated

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,6 +1,5 @@
 // const host = "https://expense-tracker-app-knl1.onrender.com";
 const host = "http://localhost:8800";
-export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/v1/user/signup`;
 export const loginAPI = `${host}/api/v1/user/signin`;
 export const addTransaction = `${host}/api/v1/addTransaction`;
